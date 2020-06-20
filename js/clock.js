c157925f5c87ef734a5b2ceb95b6faece72297f6
davinci-coding <66500511+davinci-coding@users.noreply.github.com>
2020-06-20 12:55:56 +0900

    Add files via upload

---
+++ js/clock.js
@@ -0,0 +1,20 @@
+const clock = document.querySelector(".js-clock .clock_text");
+
+function getTime(){
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const time = `${hours} : ${minutes} : ${seconds}`;
+    clock.innerHTML = time;
+    console.log(hours, time);
+    return;
+}
+
+function init(){
+    getTime();
+    setInterval(getTime, 1000);
+    return;
+}
+
+init();
